232356d717e88337f9a5ea027e321b78ee29f8c8
lucasfrezzini <luquiitas.f@gmail.com>
2021-08-15 20:28:36 -0300

    Proyecto Pugstagram v1.0.0 - First commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+    let year = new Date().getFullYear();
+</script>
+
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="Footer-copy">
+            © <span id="year">{year}</span>
+            PUGSTAGRAM FROM PLATZI
+        </div>
+    </div>
+</div>
+
+<style>
+    .Footer {
+        font-size: 11px;
+        letter-spacing: 1px;
+        font-weight: normal;
+        color: #c7c7c7;
+    }
+</style>
---
+++ src/components/Timeline.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Card from "./Card.svelte";
+
+    export let posts = [];
+
+</script>
+
+<div class="Timeline">
+    <div class="Timeline-container">
+        {#each posts as post }
+            <Card {...post}  />  
+        {:else}
+            <p>Loading...</p>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .Timeline {
+        padding: 4em 0 0 0;
+    }
+</style>
